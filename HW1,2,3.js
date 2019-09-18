b86eee3bfe23045ca19a6ac5bcdba901d0c8bb6d
cmurja <55098908+cmurja@users.noreply.github.com>
2019-09-17 20:26:26 -0400

    HW4

--- HW1,2,3.js
+++ HW1,2,3.js
@@ -132,8 +132,13 @@ todo.edit(22);
 todo.delete();
  */
 
+
+
+
+// HW 4
+
 const inventors = [
-    { first: 'Albert', last: 'Einstein', year: 1879, passed: 1955 },
+    { first: 'Albert',  last: 'Einstein', year: 1879, passed: 1955 },
     { first: 'Isaac', last: 'Newton', year: 1643, passed: 1727 },
     { first: 'Galileo', last: 'Galilei', year: 1564, passed: 1642 },
     { first: 'Marie', last: 'Curie', year: 1867, passed: 1934 },
@@ -151,11 +156,17 @@ const inventors = [
     // Array.prototype.filter()
     // 1. Filter the list of inventors for those who were born in the 1500's, use console.table to display the results
 
-    const prototype = age.filter( age =>)
-   
- 
+let limitBirth = inventors.filter(function (b) {
+    return b.year > 1500 && b.year < 1600;
+});
+console.table(limitBirth);
 
 
     // Array.prototype.map()
     // 2. Give us an array of the inventor first and last names
     //console.table the results
+
+let fullName = inventors.map (function (n) {
+    return n.first  + n.last;
+}); 
+console.table(fullName);
